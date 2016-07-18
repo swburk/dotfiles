4717064fdff61858ddd924eb546cc2e69806fa0d
Sam Burk <sam@samburk.com>
2016-07-18 15:56:19 -0500

    finished fish prompt

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,17 +1,13 @@
+set fish_greeting
 set PATH "/usr/local/bin" $PATH
-set fish_greeting ''
 set -x EDITOR nvim
 
-function tn; tmux new -s; end
-function ta; tmux attach -t; end
+function tn; tmux new -s $argv; end
+function ta; tmux attach -t $argv; end
 function tl; tmux list-sessions; end
 
 function e --wraps nvim
-    if test (count $argv) -gt 0
-        eval $EDITOR $argv
-    else
-        eval $EDITOR .
-    end
+    eval $EDITOR $argv
 end
 
 function g --wraps git
@@ -24,21 +20,39 @@ end
 
 function fish_prompt
     set -l last_status $status
+    set -l job_count (jobs | wc -l | tr -d " ")
 
-    # Show number of jobs
-    # Show username and hostname over SSH
+    # Jobs
+    if test $job_count -gt 0
+        set_color red
+        printf "$job_count "
+        set_color normal
+    end
 
+    # Username and hostname displayed in an SSH session
+    if set -q SSH_CLIENT
+        printf "%s%s%s@%s%s%s " (set_color green) (whoami) (set_color normal) \
+                                (set_color blue) (hostname | cut -d . -f 1) (set_color normal)
+    end
+
+    # Current directory
     set_color cyan
-    printf '%s' (pwd)
+    printf (pwd | sed "s#^"$HOME"#~#")
+    set_color normal
 
-    set_color purple
-    printf '%s ' (__fish_git_prompt)
+    # Current VCS branch
+    set_color magenta
+    printf "%s " (__fish_git_prompt)
+    set_color normal
 
+    # Prompt character is red when command exits with non-zero
     if test $last_status -ne 0
         set_color red
-    else
-        set_color normal
     end
     printf "> "
     set_color normal
+end
+
+function fish_right_prompt
+    date "+%H:%M:%S"
 end
