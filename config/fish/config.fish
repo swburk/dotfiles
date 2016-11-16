0e398f2677609f3e7da899a25fea41c15cd843b0
Sam Burk <sam@samburk.com>
2016-11-16 14:25:56 -0600

    added ultisnips and a useful fish function

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,7 @@ set -x EDITOR nvim
 function tn; tmux new -s $argv; end
 function ta; tmux attach -t $argv; end
 function tl; tmux list-sessions; end
+function slr; ssh -l root; end
 
 function e --wraps nvim
     eval $EDITOR $argv
