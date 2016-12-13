d9745f2ffbd1a683e240f4defb229cdf0fafc2e4
Sam Burk <sam@samburk.com>
2016-12-13 11:37:51 -0600

    added alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,6 +5,7 @@ set -x MANPAGER "nvim -c 'set ft=man' -"
 function tn; tmux new -s $argv; end
 function ta; tmux attach -t $argv; end
 function tl; tmux list-sessions; end
+function s; ssh $argv; end
 function sla; ssh -l root app.getmediapanel.com; end
 function sld; ssh -l root dev.getmediapanel.com; end
 function slr; ssh -l root $argv; end
