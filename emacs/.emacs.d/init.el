55d28b192c4d068dacc6ba93cf34a8f64af88026
Sam Burk <sburk@telmetrics.com>
2021-09-17 11:14:11 -0500

    emacs: change backup settings, remove packages, change theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,24 +1,27 @@
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
-(setq package-list
-      '(dracula-theme))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+;; (require 'package)
+;; (add-to-list 'package-archives
+;; 	     '("melpa" . "https://melpa.org/packages/") t)
+;; (setq package-list
+;;       '())
+;; (package-initialize)
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
+;; (dolist (package package-list)
+;;   (unless (package-installed-p package)
+;;     (package-install package)))
 
-(setq-default
+(setq
  visible-bell t
  inhibit-startup-message t
  initial-scratch-message ""
- custom-file (concat user-emacs-directory "/custom.el"))
+ backup-directory-alist `((".*" . ,(concat user-emacs-directory "backups")))
+ backup-by-copying t
+ delete-old-versions t
+ version-control t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (blink-cursor-mode 0)
-(load-theme 'dracula t)
+(load-theme 'deeper-blue)
 (when (member "Source Code Pro" (font-family-list))
   (set-frame-font "Source Code Pro-10" t t))
