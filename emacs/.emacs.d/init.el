987cef3ffb8903407fffc9c2bbf0537ade05d2cd
Sam Burk <sburk@telmetrics.com>
2021-09-17 00:23:42 -0500

    Add packages and dracula theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,11 +1,24 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(setq package-list
+      '(dracula-theme))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
 (setq-default
  visible-bell t
  inhibit-startup-message t
- initial-scratch-message "")
+ initial-scratch-message ""
+ custom-file (concat user-emacs-directory "/custom.el"))
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (blink-cursor-mode 0)
-(load-theme 'tango-dark)
+(load-theme 'dracula t)
 (when (member "Source Code Pro" (font-family-list))
   (set-frame-font "Source Code Pro-10" t t))
