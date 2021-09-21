faba4030a05312cd04c788609eec7e8cb913eae4
Sam Burk <sburk@telmetrics.com>
2021-09-21 17:15:37 -0500

    Added evil and org to emacs and reorganized init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,23 +1,19 @@
-;; (require 'package)
-;; (add-to-list 'package-archives
-;; 	     '("melpa" . "https://melpa.org/packages/") t)
-;; (setq package-list
-;;       '())
-;; (package-initialize)
-;; (unless package-archive-contents
-;;   (package-refresh-contents))
-;; (dolist (package package-list)
-;;   (unless (package-installed-p package)
-;;     (package-install package)))
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+(require 'evil)
+(evil-mode 1)
 
 (setq
  visible-bell t
  inhibit-startup-message t
- initial-scratch-message ""
- backup-directory-alist `((".*" . ,(concat user-emacs-directory "backups")))
- backup-by-copying t
- delete-old-versions t
- version-control t)
+ initial-scratch-message "")
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -25,3 +21,19 @@
 (load-theme 'deeper-blue)
 (when (member "Source Code Pro" (font-family-list))
   (set-frame-font "Source Code Pro-10" t t))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file :noerror)
+
+(setq
+ backup-directory-alist `((".*" . ,(expand-file-name "backups" user-emacs-directory)))
+ backup-by-copying t
+ delete-old-versions t
+ version-control t)
+
+(global-set-key (kbd "M-o") 'mode-line-other-buffer)
+
+(setq org-log-done 'time)
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
