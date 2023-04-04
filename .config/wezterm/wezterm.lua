bbb367aca5dc3ba2f7fc03b621b9b3989c9247bf
Sam Burk <sam@samburk.com>
2023-04-04 15:01:51 -0500

    Added wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Basic'
+config.font = wezterm.font 'Source Code Pro'
+config.font_size = 14
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.quit_when_all_windows_are_closed = false
+config.window_close_confirmation = 'NeverPrompt'
+
+return config
