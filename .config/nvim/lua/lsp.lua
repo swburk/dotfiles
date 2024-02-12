949b64033bb75cafa2ff8eb672443b51e5ab5a48
Sam Burk <sam@samburk.com>
2024-02-12 09:43:30 -0600

    Enable LSP formatter, stop using pylsp, use ruff & pyright

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,54 +1,48 @@
 local lspconfig = require('lspconfig')
 lspconfig.eslint.setup({})
 lspconfig.gopls.setup({})
-lspconfig.pylsp.setup({
-	settings = {
-		pylsp = {
-			plugins = {
-				flake8 = {
-					enabled = true,
-					ignore = { 'E402' },
-					maxLineLength = 100,
-				},
-				autopep8 = { enabled = false },
-				yapf = { enabled = false },
-				pyflakes = { enabled = false },
-				pycodestyle = { enabled = false },
-			},
-		},
-	},
-})
+lspconfig.pyright.setup({})
+lspconfig.ruff_lsp.setup({})
 lspconfig.tsserver.setup({})
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client.server_capabilities.hoverProvider then
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		local opts = { buffer = ev.buf }
+		if client.supports_method('textDocument/hover') then
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 		end
-		if client.server_capabilities.signatureHelpProvider then
-			vim.keymap.set({ 'n', 'i' }, '<c-k>', vim.lsp.buf.signature_help, { buffer = args.buf })
+		if client.supports_method('textDocument/signatureHelp') then
+			vim.keymap.set({ 'n', 'i' }, '<c-k>', vim.lsp.buf.signature_help, opts)
 		end
-		if client.server_capabilities.declarationProvider then
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = args.buf })
+		if client.supports_method('textDocument/declaration') then
+			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
 		end
-		if client.server_capabilities.definitionProvider then
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf })
+		if client.supports_method('textDocument/definition') then
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 		end
-		if client.server_capabilities.typeDefinitionProvider then
-			vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, { buffer = args.buf })
+		if client.supports_method('textDocument/typeDefinition') then
+			vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
 		end
-		if client.server_capabilities.implementationProvider then
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = args.buf })
+		if client.supports_method('textDocument/implementation') then
+			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 		end
-		if client.server_capabilities.referencesProvider then
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = args.buf })
+		if client.supports_method('textDocument/references') then
+			vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 		end
-		if client.server_capabilities.renameProvider then
-			vim.keymap.set('n', 'cr', vim.lsp.buf.rename, { buffer = args.buf })
+		if client.supports_method('textDocument/rename') then
+			vim.keymap.set('n', 'cr', vim.lsp.buf.rename, opts)
 		end
-		if client.server_capabilities.codeActionProvider then
-			vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, { buffer = args.buf })
+		if client.supports_method('textDocument/codeAction') then
+			vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, opts)
+		end
+		if client.supports_method('textDocument/formatting') then
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				buffer = ev.buf,
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
 		end
 	end,
 })
