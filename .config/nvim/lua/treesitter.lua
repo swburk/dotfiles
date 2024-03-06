5ffa6bb598a40e492dfdb9454d8c73ddf4dd2a62
Sam Burk <sam@samburk.com>
2024-03-06 12:30:25 -0600

    Added more treesitter text objects

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -26,18 +26,68 @@ require('nvim-treesitter.configs').setup {
 		select = {
 			enable = true,
 			keymaps = {
-				['af'] = '@function.outer',
-				['if'] = '@function.inner'
-			}
+				["ae"] = "@assignment.outer",
+				["ie"] = "@assignment.inner",
+
+				["aa"] = "@parameter.outer",
+				["ia"] = "@parameter.inner",
+
+				["ai"] = "@conditional.outer",
+				["ii"] = "@conditional.inner",
+
+				["al"] = "@loop.outer",
+				["il"] = "@loop.inner",
+
+				["af"] = "@call.outer",
+				["if"] = "@call.inner",
+
+				["am"] = "@function.outer",
+				["im"] = "@function.inner",
+
+				["at"] = "@class.outer",
+				["it"] = "@class.inner",
+			},
 		},
 		move = {
 			enable = true,
 			goto_next_start = {
+				[']e'] = '@assignment.outer',
+				[']a'] = '@parameter.outer',
+				[']i'] = '@conditional.outer',
+				[']l'] = '@loop.outer',
+				[']f'] = '@call.outer',
 				[']m'] = '@function.outer',
+				[']t'] = '@class.outer',
+				[']/'] = '@comment.outer',
+			},
+			goto_next_end = {
+				[']E'] = '@assignment.outer',
+				[']A'] = '@parameter.outer',
+				[']I'] = '@conditional.outer',
+				[']L'] = '@loop.outer',
+				[']F'] = '@call.outer',
+				[']M'] = '@function.outer',
+				[']T'] = '@class.outer',
 			},
 			goto_previous_start = {
-				['[m'] = '@function.outer'
-			}
+				['[e'] = '@assignment.outer',
+				['[a'] = '@parameter.outer',
+				['[i'] = '@conditional.outer',
+				['[l'] = '@loop.outer',
+				['[f'] = '@call.outer',
+				['[m'] = '@function.outer',
+				['[t'] = '@class.outer',
+				['[/'] = '@comment.outer',
+			},
+			goto_previous_end = {
+				['[E'] = '@assignment.outer',
+				['[A'] = '@parameter.outer',
+				['[I'] = '@conditional.outer',
+				['[L'] = '@loop.outer',
+				['[F'] = '@call.outer',
+				['[M'] = '@function.outer',
+				['[T'] = '@class.outer',
+			},
 		}
 	}
 }
