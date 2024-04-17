535716473e7d72120838f61f5d84ea10b858e9ad
Sam Burk <sam@samburk.com>
2024-04-17 08:33:28 -0500

    Change statusline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,6 +21,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0
 vim.opt.shiftround = true
 vim.opt.number = true
+vim.opt.statusline = '%<%f %h%w%m%r%{FugitiveStatusline()}%=%l:%v/%L %y'
 vim.cmd.colorscheme('catppuccin')
 
 vim.diagnostic.config({
