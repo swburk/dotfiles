35220480ac5c7c60ce25f85bb2c2493c0381e11c
Sam Burk <sam@samburk.com>
2024-04-19 15:34:27 -0500

    Adjust alt buffer mapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,7 +33,7 @@ vim.keymap.set('n', '<Plug>(SplitLine)',
 	[[<cmd>keeppatterns substitute/\v^(\s*)(.{-})\s*%#\s*(.*)/\1\2\r\1\3/|]]
 	.. [[call repeat#set("\<Plug>(SplitLine)")<cr>]], { noremap = true })
 vim.keymap.set('n', 'S', '<Plug>(SplitLine)')
-vim.keymap.set('n', '<tab>', '<c-^>', { noremap = true })
+vim.keymap.set('n', '<tab>', '<cmd>buffer #<cr>', { noremap = true })
 vim.keymap.set('n', 'gV', '`[v`]', { noremap = true })
 vim.keymap.set('n', 'gs', ':%substitute///g<left><left>', { noremap = true })
 vim.keymap.set('x', 'gs', ':substitute///g<left><left>', { noremap = true })
